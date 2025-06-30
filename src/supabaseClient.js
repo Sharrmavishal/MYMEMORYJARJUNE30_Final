c0e4e3c2c1656cb6e1308e10bd708fdad38202ff
Sharrmavishal <113687802+Sharrmavishal@users.noreply.github.com>
2025-06-30 17:52:00 +0530

    Deleted supabaseClient.js

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://tzfvteccfyxfefwhmara.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6ZnZ0ZWNjZnl4ZmVmd2htYXJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNjUxMjUsImV4cCI6MjA2Njg0MTEyNX0.2V5VDPPGTxo4R-_s-e7eBUEc7MF8N0s-9zdsZpCNFgY'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
